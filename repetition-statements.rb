cbb81276c692a9548c456e2b602a2bbdf5f070e5
Michelle Tanzil <mtanzil01@gmail.com>
2019-09-08 18:43:30 +0000

    Automatically backed up by Learn

---
+++ repetition-statements.rb
@@ -0,0 +1,31 @@
+magic_exit_number = 7 
+count = 0 
+while count < 10 && count !=magic_exit_number do 
+  puts "I am the #{count}, I love to count!"
+  count += 1 
+end
+
+n = 2 
+count = 0 
+while count <= n do
+  puts "I ran."
+  count += 1 
+end
+
+3.times do 
+  puts "I ran."
+end
+
+count = 0 
+n = 3 
+loop do 
+  break if count >= n 
+  puts "I ran."
+  count += 1 
+end
+
+counter = 0 
+until counter == 20
+  puts "The current number is less than 20."
+  counter += 1 
+end
